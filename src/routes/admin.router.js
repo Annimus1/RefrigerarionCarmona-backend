cfc04699bd851c5b62d56940f866ad6d4320b836
pablo <pablo.j98@gmail.com>
2024-10-24 13:31:43 -0400

    add getRoleByToken

--- src/routes/admin.router.js
+++ src/routes/admin.router.js
@@ -1,18 +1,18 @@
 import express from "express";
-import { createAdmin, getUserById, getUsers } from '../DB/db.js';
+import { createAdmin, getUserById, getUsers, removeToken } from '../DB/db.js';
 import { getPayload } from "../auth/auth.js";
 
 const router = express.Router();
 
 // USERS
 // create a new user (admin)
-router.post("/createAdmin", async (req, res)=>{
+router.post("/createAdmin", async (req, res) => {
 	// get the token and extract payload
 	const token = req.get("Authorization").split(" ")[1];
 	const payload = getPayload(token);
 
 	// only admin can create an admin user.
-	if(payload.role !== 'admin') res.status(401).send();
+	if (payload.role !== 'admin') res.status(401).send();
 
 	// get information from client
 	const body = req.body;
@@ -24,17 +24,17 @@ router.post("/createAdmin", async (req, res)=>{
 
 	// save new user in database
 	let result = await createAdmin(body);
-	
-	if(result == 0){
+
+	if (result == 0) {
 		console.log(payload.name, "ha creado un nuevo usuario administador: ", body.name);
-		
+
 		// create a new user in table
 		res.status(201).send(JSON.stringify(body));
 	}
-	else{
+	else {
 		res.status(403).send("User Already exists.");
 	}
-	
+
 });
 
 // get all users
@@ -46,31 +46,61 @@ router.get("/users", async (req, res) => {
 	// get aditional information
 	// expected a role in query params
 	const params = req.query;
-    
+
 	// get users
 	const users = await getUsers(payload, params?.role);
-    
-	if(users){
+
+	if (users) {
 		res.send(JSON.stringify(users));
-	}else{
+	} else {
 		res.status(401).send()
 	}
 });
 
 // get user by id
 router.get("/users/:id", async (req, res) => {
-    // get the token and extract payload
+	// get the token and extract payload
 	const token = req.get("Authorization").split(" ")[1];
 	const payload = getPayload(token);
-    
+
 	// get user
 	const user = await getUserById(payload, req.params.id);
 
-	if(user){
+	if (user) {
 		res.send(JSON.stringify(user));
-	}else{
+	} else {
 		res.status(401).send()
 	}
+});
+
+// get role by token
+router.get("/getRole", async (req, res) => {
+	// get the token 
+	const token = req.get("Authorization").split(" ")[1];
+	console.log(token)
+	try {
+		// extract payload
+		const payload = getPayload(token);
+		// return role
+		res.status(200).send(JSON.stringify({role:payload.role}))
+	} catch (e) {
+
+		// expired token
+		if (e instanceof jwt.TokenExpiredError) {
+			removeToken(token);
+			console.error(`Token expired.`);
+			res.status(403).redirect(`${process.env.FRONTEND_DOMAIN}/login`)
+		}
+		
+		// invalid format token
+		if (e instanceof jwt.JsonWebTokenError) {
+			removeToken(token);
+			console.error(`invalid token format ${token}.`);
+			res.status(403).redirect(`${process.env.FRONTEND_DOMAIN}/login`)
+		}
+		console.log(e);
+	}
+
 });
 
 // WARRANTY
